2ff491e98e61d2851f599023a9f8b622e14aaab2
James Yu <jamesyu717@163.com>
2017-12-26 15:48:02 +0800

    add code of baidu tongji

---
+++ assets/js/baidu_analytics.js
@@ -0,0 +1,10 @@
+<!-- yujie24.github.io Baidu tongji analytics -->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?976076bea443c7245e28ffcecb3439b4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
